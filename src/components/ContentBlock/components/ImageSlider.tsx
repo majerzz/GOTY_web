8f453676c7304f8b6d7c5d0acc25e40038316fa1
majerzz <juliamjrzzz@gmail.com>
2024-01-28 19:10:36 +0600

    slider functional v1.0

--- src/components/ContentBlock/components/ImageSlider.tsx
+++ src/components/ContentBlock/components/ImageSlider.tsx
@@ -1,13 +1,44 @@
 import { cx } from '@emotion/css'
+import { useState } from 'react'
 import { styles } from 'src/components/ContentBlock/components/ImageSlider.styles'
-import img from 'src/img/bg-screen1.jpg'
+import img1 from 'src/img/bg-screen1.jpg'
+import img2 from 'src/img/bg-screen1.jpg'
+import img3 from 'src/img/bg-screen1.jpg'
+import img4 from 'src/img/123123213.jpg'
 
-export const ImageSlider:React.FC = () => {
-  return(
+const images = [img1, img2, img3, img4]
+// currentImage, prevImage, nextImage
+// onClick ф-ция (direction) => {direction === right ? setImage(nextImage) : setImage(prevImage)}
+
+export const ImageSlider: React.FC = () => {
+
+  const [currentImage, setCurrentImage] = useState(0)
+  const prev = currentImage === 0 ? images.length - 1 : currentImage - 1;
+  const next = currentImage === images.length - 1 ? 0 : currentImage + 1;
+
+  const moveSlider = (direction: 'left' | 'right') => {
+    if (direction === 'left') {
+      setCurrentImage(prev);
+    } else {
+      setCurrentImage(next);
+    }
+  }
+
+  return (
     <div className={styles.imageSlider}>
-      <img className={cx(styles.imageSliderItem, styles.prev)} src={img} alt="fdsdfs" />
-      <img className={cx(styles.imageSliderItem, styles.active)} src={img} alt="fdsdfs" />
-      <img className={cx(styles.imageSliderItem, styles.next)} src={img} alt="fdsdfs" />
+      {images.map((image, index) => {
+        let type = '';
+        if (index === currentImage) {
+          type = styles.active
+        } else if (index === prev) {
+          type = styles.prev
+        } else if (index === next) {
+          type = styles.next
+        }
+        return (<img className={cx(styles.imageSliderItem, type)} src={image} key={index} alt="fdsdfs" />)
+      })}
+      <div className={cx(styles.button, styles.buttonLeft)} onClick={() => moveSlider('left')}></div>
+      <div className={cx(styles.button, styles.buttonRight)} onClick={() => moveSlider('right')}></div>
     </div>
   )
 }
